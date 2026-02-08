59f57a6c4496c864d35fb472485d477441b3dc5a
Oleksii Hryshchenko <109420678+OlexaGryschenko@users.noreply.github.com>
2026-02-08 19:21:54 +0000

    Add files via upload

---
+++ python-9/01lesson.py
@@ -0,0 +1,21 @@
+name = "Софія" # Text string
+age = 18 # integer
+height = 1.85 # float
+is_student = True # Logical (boolean - true , folse)
+empty = ' '
+
+#print
+
+score = 10
+print(score)
+score = score + 5 # слева 
+print(score)
+
+print('Мені ', age,'років')
+
+height = 1.70
+print("Мій зріст дорівнює", height, "m")
+
+
+
+
